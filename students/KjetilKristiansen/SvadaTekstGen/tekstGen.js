ae6b27f3c10afdf9c30455522b8e17d3cb123c30
kjetkr01 <kjetil2001@gmail.com>
2020-09-07 18:41:21 +0200

    Update tekstGen.js

--- students/KjetilKristiansen/SvadaTekstGen/tekstGen.js
+++ students/KjetilKristiansen/SvadaTekstGen/tekstGen.js
@@ -1,4 +1,13 @@
+const tekster = ["Avsnitt 1", "Avsnitt 2", "Avsnitt 3", "Avsnitt 4", "Avsnitt 5"];
 
+const antallTekster = tekster.length;
+userInp = process.argv[2];
+let antallAvsnitt = userInp;
 
-let tekster = ["Tekst 1", "Tekst 2", "Tekst 3"];
-
+if(userInp <= antallTekster){
+    for(let i = 0; i < antallAvsnitt; i++){
+        console.log(tekster[i]);
+    }
+    return;
+}
+else {console.log("Input has to be min: 1 or max: " + antallTekster);}
