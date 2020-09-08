3c30512ea16fffa65710429dc4aa9a10bfac83bf
kjetkr01 <kjetil2001@gmail.com>
2020-09-08 14:07:58 +0200

    Update nedtelling.js

--- students/KjetilKristiansen/Nedtelling/nedtelling.js
+++ students/KjetilKristiansen/Nedtelling/nedtelling.js
@@ -1,9 +1,28 @@
-let h = document.getElementById("inpHours");
 
-let m = "";
-let s = "";
-let msg = "";
 
-function test(){
-    console.log(h);
+
+
+
+function startCountdown(){
+    let h = document.getElementById("inpHours").value;
+    let m = document.getElementById("inpMinutes").value;
+    let s = document.getElementById("inpSeconds").value;
+    let msg = document.getElementById("inpMessage").value;
+    console.log("Hours: " + h);
+    console.log("Minutes: " + m);
+    console.log("Seconds: " + s);
+    console.log("Message: " + msg);
+
+    countDown();
+
+    
+}
+
+let countDownTimer = 5;
+
+function countDown(){
+
+    for(let i = 0; i < countDownTimer; i++){
+        document.getElementById("nedTelling").innerHTML = "Countdown .... ";
+    }
 }
