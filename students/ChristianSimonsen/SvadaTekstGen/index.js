9bff0dd3e22439b2d7294be7a3c55673c2ca45c2
Christian Simonsen <christian@agens.no>
2020-09-08 09:46:50 +0200

    Litt rydding etter forelesning

--- students/ChristianSimonsen/SvadaTekstGen/index.js
+++ students/ChristianSimonsen/SvadaTekstGen/index.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 
-/*let svadatekst = [
-    "Det skyldes først og fremst at lærere er i karantene, og at vi har så få igjen at det beste tilbudet vi kan gi er hjemmeundervisning, sa oppvekstdirektør Erik Bråthen i Sarpsborg kommune.",
-    "Utdanningsforbundet vil stenge alle skolene i Sarpsborg etter smitteutviklingen i området.",
-    "Under de rådende omstendigheter med smittespredningen som er i Sarpsborg, anbefaler vi å stenge ned alle skoler inntil vi har full kontroll på situasjonen, sier Knut Olav Farbrot, leder i Utdanningsforbundet i Sarpsborg, til Sarpsborg Arbeiderblad."
-]*/
-
-let svadatekst = fs.readFileSync("tekster.txt").split("\n");
-
-//const antallAvsnitt = process.argv.length >=2 ? process.argv[2]:3;
+let svadatekst = fs.readFileSync("tekster.txt", "utf-8").split("\n");
 
 let antallAvsnitt = svadatekst.length;
 if (process.argv.length >= 3) {
