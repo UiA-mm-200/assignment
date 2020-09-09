efc6ba0be53fc6fadd7da5ebd47643a89aa01c58
lenellem <leneb98@hotmail.com>
2020-09-09 12:31:43 +0200

    Update GjettTall.js

--- students/LeneBekkemellem/GjettTall/GjettTall.js
+++ students/LeneBekkemellem/GjettTall/GjettTall.js
@@ -5,29 +5,47 @@ const rl = readline.createInterface({
 }); 
 
 let number = 5; 
-let counter = 0; 
 
-rl.question ("Gjett et tall fra 1-10", function(input){
-    if (input > number){
-        console.log ("Du må gjette lavere"); 
-    } else if (input < number) {
-        console.log("Du må gjette høyere"); 
 
-    } else if (input == number) {
-     console.log("Gratulerer du gjettet riktig!"); 
-}
-counter++; 
-console.log("du har gjettet " + counter + " /n ganger"); 
+guess(); 
+let counter = 1; 
+ 
 
-rl.question ("Do you want to play again?(yes or no)", function (input){
-    if (input == "yes"){
-        rl.resume
-    }
-    if (input == "no"){
+function guess () {
+    rl.question ("Gjett et tall fra 1-10: ", function(input){
+        
+        if (input > number){
+            console.log ("Du må gjette lavere");
+            playAgain();  
+        } else if (input < number) {
+            console.log("Du må gjette høyere"); 
+            playAgain(); 
+    
+        } else if (input == number) {
+         console.log("Gratulerer du gjettet riktig!");
+         console.log("du har gjettet " + counter + " ganger");
         rl.close(); 
+
     }
-    rl.close(); 
+    counter++;
 }); 
+     
+}
+
+function playAgain(){
+    rl.question ("Do you want to play again?(yes or no)", function (input){
+
+        if (input == "yes"){
+            guess(); 
+        }
+        if (input == "no"){
+            rl.close(); 
+        }
+        
+    }); 
    
      
-}); 
+}
+
+   
+     
