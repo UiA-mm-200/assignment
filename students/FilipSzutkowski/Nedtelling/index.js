8d2558eb9d4205755c37513f5f907321085d0ad5
FilipSzutkowski <filip99.ski@gmail.com>
2020-09-10 14:12:50 +0200

    Logic calculating time created

--- students/FilipSzutkowski/Nedtelling/index.js
+++ students/FilipSzutkowski/Nedtelling/index.js
@@ -1,11 +1,11 @@
 // Variables ---------------------------------------------------
 
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-let dateObject = new Date();
+let dateObject = new Date('August 19, 1975');
+let startTime = new Date('August 19, 1975');
+let userHours = isNaN(grabFlag(`--hours`)) ? 0 : grabFlag(`--hours`);
+let userMinutes = isNaN(grabFlag(`--minutes`)) ? 0 : grabFlag(`--minutes`);
+let userSeconds = isNaN(grabFlag(`--seconds`)) ? 0 : grabFlag(`--seconds`);
+let intervalTimeMs = 0;
 
 // Function calls
 
@@ -14,14 +14,28 @@ timeInterval();
 // Function declaratiosn ---------------------------------------------------
 
 function timeInterval() {
-    const interval = setInterval(displayTime, 1000);
+    const interval = setInterval(() => {
+        displayTime(userHours, userMinutes, userSeconds)
+    }, 1000);
+
+    intervalTimeMs = (userHours * 3600000) + (userMinutes * 60000) + (userSeconds * 1000);
+
+    startTime.setTime(Date.now());
+
     setTimeout( () => {
         clearInterval(interval);
         process.exit(0);
-    }, 10000);
+    }, intervalTimeMs);
+
+    console.log(intervalTimeMs);
 }
 
-function displayTime() {
-    dateObject.setTime(Date.now());
-    console.log(dateObject);
+function displayTime(hours, minutes, seconds) {
+    dateObject.setTime(Date.now() - startTime);
+    console.log(`${hours - dateObject.getUTCHours()}:${minutes - dateObject.getUTCMinutes()}:${seconds - dateObject.getUTCSeconds()}`);
+}
+
+function grabFlag(flag) {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1; 
+    return process.argv[indexAfterFlag];
 }
