08571b3bf8b5ae3330a5b8886c61641fed31e90b
LarsOHelaas <70975771+LarsOHelaas@users.noreply.github.com>
2020-09-13 21:34:29 +0200

    Oppdatert roman number generator
    Slettet gamel kode som kun kunne konvertere basert på arrays på 1-10 og lagde en funksjon som kan lage tall opp mot 5000. Håper det er greit.

--- students/LarsOlafHelaas/ConvertRoman/roman.js
+++ students/LarsOlafHelaas/ConvertRoman/roman.js
@@ -1,7 +1,22 @@
-let wANumerals = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-let rNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]; 
+let input; 
 
-let index = wANumerals.indexOf(process.argv[2]); 
-let activeIndex = index + 1; 
+if (process.argv.length >= 3 && process.argv[2] <5000 && process.argv[2] > 0) {
+    input = process.argv[2];
+} else {
+    console.log("Please enter a number between 1 and 4999")
+}
 
-console.log("Romertall for " + activeIndex + " = " + rNumerals[index]); 
+function makeRoman(number) {
+    let lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+    let roman = ''
+    let i;
+  for ( i in lookup ) {
+    while ( number >= lookup[i] ) {
+      roman += i;
+      number -= lookup[i];
+    }
+  }
+  return roman;
+}
+
+console.log(makeRoman(input));
